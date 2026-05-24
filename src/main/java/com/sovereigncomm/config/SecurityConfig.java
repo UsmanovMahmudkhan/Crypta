e26ce90545b1861b19ada48e14d5f8c5997950c3
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:10:17 +0000

    add SecurityConfig lines 5-9

--- src/main/java/com/sovereigncomm/config/SecurityConfig.java
+++ src/main/java/com/sovereigncomm/config/SecurityConfig.java
@@ -2,3 +2,8 @@ package com.sovereigncomm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
