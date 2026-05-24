c9268195613fef0b6a5513ddd3dfde8023d34a12
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:09:01 +0000

    add SovereignCommApplication lines 5-9

--- src/main/java/com/sovereigncomm/SovereignCommApplication.java
+++ src/main/java/com/sovereigncomm/SovereignCommApplication.java
@@ -2,3 +2,8 @@ package com.sovereigncomm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovereignCommApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SovereignCommApplication.class, args);
