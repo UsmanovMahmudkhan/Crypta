904485fc091f56b2a76a062785c5390600a94149
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:09:05 +0000

    add AdminController lines 10-14

--- src/main/java/com/sovereigncomm/api/AdminController.java
+++ src/main/java/com/sovereigncomm/api/AdminController.java
@@ -7,3 +7,8 @@ import com.sovereigncomm.service.AuditService;
 import com.sovereigncomm.service.EmergencyLockdownService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
