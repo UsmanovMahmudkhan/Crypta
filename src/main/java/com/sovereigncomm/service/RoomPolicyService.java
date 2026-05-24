89b19792438ffa6ec7f8e233a069b67da170f185
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:11:38 +0000

    add RoomPolicyService lines 6-7

--- src/main/java/com/sovereigncomm/service/RoomPolicyService.java
+++ src/main/java/com/sovereigncomm/service/RoomPolicyService.java
@@ -3,3 +3,5 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.RoomPolicyUpdateRequest;
 
 public interface RoomPolicyService {
+    void updatePolicy(RoomPolicyUpdateRequest request);
+}
