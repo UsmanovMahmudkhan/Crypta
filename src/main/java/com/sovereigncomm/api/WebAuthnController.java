78d05efa3096fd807f57a660836af97f8f230593
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:02 +0900

    add web authn controller imports

---
+++ src/main/java/com/sovereigncomm/api/WebAuthnController.java
@@ -0,0 +1,5 @@
+package com.sovereigncomm.api;
+
+import com.sovereigncomm.api.dto.CommonDtos.WebAuthnFinishRequest;
+import com.sovereigncomm.api.dto.CommonDtos.WebAuthnStartResponse;
+import com.sovereigncomm.service.AuthService;
