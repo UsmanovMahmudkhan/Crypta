384c365910f54269e91e8ad1d5e728d59cf60f4d
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:33 +0900

    define secure storage provider contract

--- src/main/java/com/sovereigncomm/crypto/SecureStorageProvider.java
+++ src/main/java/com/sovereigncomm/crypto/SecureStorageProvider.java
@@ -2,3 +2,5 @@ package com.sovereigncomm.crypto;
 
 public interface SecureStorageProvider {
     byte[] wrapKey(String hardwareKeyAlias, byte[] keyMaterial);
+    byte[] unwrapKey(String hardwareKeyAlias, byte[] wrappedKey);
+}
