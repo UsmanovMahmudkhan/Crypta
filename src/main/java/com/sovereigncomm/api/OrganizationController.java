0d2498940b227a1143679a7cb2d18787af1946d6
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:42:50 +0900

    add organization controller imports

---
+++ src/main/java/com/sovereigncomm/api/OrganizationController.java
@@ -0,0 +1,5 @@
+package com.sovereigncomm.api;
+
+import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
+import com.sovereigncomm.api.dto.CommonDtos.OrganizationCreateRequest;
+import com.sovereigncomm.service.OrganizationService;
