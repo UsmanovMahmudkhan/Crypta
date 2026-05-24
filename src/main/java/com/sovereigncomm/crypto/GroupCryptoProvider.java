4212b959616e30d1c5ea31fe87bed3b08efa9564
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:29 +0900

    add group crypto provider methods

--- src/main/java/com/sovereigncomm/crypto/GroupCryptoProvider.java
+++ src/main/java/com/sovereigncomm/crypto/GroupCryptoProvider.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.crypto;
 import java.util.List;
 import java.util.Map;
 
+public interface GroupCryptoProvider {
+    byte[] createGroupCommit(String roomId, List<String> memberDeviceIds);
+    byte[] encryptGroupMessage(String roomId, byte[] plaintext, Map<String, byte[]> associatedData);
+    byte[] decryptGroupMessage(String roomId, byte[] ciphertextEnvelope, Map<String, byte[]> associatedData);
+}
