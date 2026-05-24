7f9a1a308dfa6095a42b73d0be8c61b7e036e332
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-25 00:28:35 +0900

    refactor DirectMessageService for clearer logging

--- src/main/java/com/sovereigncomm/service/DirectMessageService.java
+++ src/main/java/com/sovereigncomm/service/DirectMessageService.java
@@ -1,5 +1,16 @@
 package com.sovereigncomm.service;
 
+import com.sovereigncomm.api.dto.CommonDtos.DeliveryReceiptRequest;
+import com.sovereigncomm.api.dto.CommonDtos.EncryptedMessageResponse;
+import com.sovereigncomm.api.dto.CommonDtos.EncryptedMessageSubmitRequest;
+import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
+import com.sovereigncomm.api.dto.CommonDtos.MessageInboxRequest;
+
+import java.util.List;
+
 public interface DirectMessageService {
     void acceptCiphertextEnvelope(String encryptedDirectMessageEnvelope);
+    IdResponse submit(EncryptedMessageSubmitRequest request);
+    List<EncryptedMessageResponse> inbox(MessageInboxRequest request);
+    void recordReceipt(DeliveryReceiptRequest request);
 }
