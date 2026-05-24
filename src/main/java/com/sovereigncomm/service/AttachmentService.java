8c2da0b01497154cb20ca21439e70745bc082bb3
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:37 +0900

    add attachment service methods

--- src/main/java/com/sovereigncomm/service/AttachmentService.java
+++ src/main/java/com/sovereigncomm/service/AttachmentService.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.AttachmentCreateRequest;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 
+import java.util.Map;
+import java.util.UUID;
+
+public interface AttachmentService {
+    IdResponse createEncryptedAttachment(AttachmentCreateRequest request);
