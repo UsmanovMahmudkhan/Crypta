b2ed762b159b555b605f41d904fee81ad81ba78d
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:09:41 +0000

    add RoomController lines 6-10

--- src/main/java/com/sovereigncomm/api/RoomController.java
+++ src/main/java/com/sovereigncomm/api/RoomController.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.api;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.api.dto.CommonDtos.MemberChangeRequest;
 import com.sovereigncomm.api.dto.CommonDtos.RoomCreateRequest;
+import com.sovereigncomm.api.dto.CommonDtos.RoomPolicyUpdateRequest;
+import com.sovereigncomm.service.MLSGroupService;
+import com.sovereigncomm.service.RoomPolicyService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
