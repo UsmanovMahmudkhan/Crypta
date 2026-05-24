2194a506ade7b3b6cd82cf005cbbea0b5a2c7682
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:08:43 +0000

    add mobile crypto provider contracts lines 6-10

--- mobile/lib/src/domain/crypto_provider_contracts.dart
+++ mobile/lib/src/domain/crypto_provider_contracts.dart
@@ -3,3 +3,8 @@ import 'dart:typed_data';
 abstract interface class DirectCryptoProvider {
   Future<Uint8List> encryptDirectMessage({
     required String recipientDeviceId,
+    required Uint8List plaintext,
+    required Map<String, Uint8List> associatedData,
+  });
+
+  Future<Uint8List> decryptDirectMessage({
