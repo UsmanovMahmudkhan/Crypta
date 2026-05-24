4911cb8f9384718d806d16f88b726039ea9775c9
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:42:59 +0900

    define user controller

--- src/main/java/com/sovereigncomm/api/UserController.java
+++ src/main/java/com/sovereigncomm/api/UserController.java
@@ -13,3 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
