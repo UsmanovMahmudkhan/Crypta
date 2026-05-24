e941147575e3fa04f88bf73a28da99d7c1f90402
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:10:48 +0000

    add EmergencyLockdownService lines 6-10

--- src/main/java/com/sovereigncomm/service/EmergencyLockdownService.java
+++ src/main/java/com/sovereigncomm/service/EmergencyLockdownService.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.EmergencyLockdownRequest;
 
 import java.util.UUID;
+
+public interface EmergencyLockdownService {
+    void startLockdown(EmergencyLockdownRequest request);
+    void endLockdown(UUID lockdownId, String reason);
+}
