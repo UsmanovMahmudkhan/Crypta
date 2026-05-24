e67507f816c53017805c073c26f551e5d9cba651
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:09:19 +0000

    add DeviceController lines 1-5

---
+++ src/main/java/com/sovereigncomm/api/DeviceController.java
@@ -0,0 +1,5 @@
+package com.sovereigncomm.api;
+
+import com.sovereigncomm.api.dto.CommonDtos.DeviceRegisterRequest;
+import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
+import com.sovereigncomm.service.DeviceTrustService;
