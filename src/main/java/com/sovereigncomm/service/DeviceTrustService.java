1eb8b86ff12d7f3aab658f5538cca7031fe52953
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:45 +0900

    define device trust service contract

--- src/main/java/com/sovereigncomm/service/DeviceTrustService.java
+++ src/main/java/com/sovereigncomm/service/DeviceTrustService.java
@@ -2,3 +2,8 @@ package com.sovereigncomm.service;
 
 import com.sovereigncomm.api.dto.CommonDtos.DeviceRegisterRequest;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
+
+import java.util.UUID;
+
+public interface DeviceTrustService {
+    IdResponse registerDevice(DeviceRegisterRequest request);
