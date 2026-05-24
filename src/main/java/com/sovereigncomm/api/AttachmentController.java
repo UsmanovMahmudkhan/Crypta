a7b28373713d7373b17c018247ace25d151d1f0f
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:42:38 +0900

    add attachment controller imports

--- src/main/java/com/sovereigncomm/api/AttachmentController.java
+++ src/main/java/com/sovereigncomm/api/AttachmentController.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.api;
 import com.sovereigncomm.api.dto.CommonDtos.AttachmentCreateRequest;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.service.AttachmentService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
