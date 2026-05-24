1588e5613b9d43d6c1e57de55d5810d5233fe929
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:25 +0900

    add direct crypto provider methods

--- src/main/java/com/sovereigncomm/crypto/DirectCryptoProvider.java
+++ src/main/java/com/sovereigncomm/crypto/DirectCryptoProvider.java
@@ -3,3 +3,6 @@ package com.sovereigncomm.crypto;
 import java.util.Map;
 
 public interface DirectCryptoProvider {
+    byte[] encryptDirectMessage(String recipientDeviceId, byte[] plaintext, Map<String, byte[]> associatedData);
+    byte[] decryptDirectMessage(String senderDeviceId, byte[] ciphertextEnvelope, Map<String, byte[]> associatedData);
+}
