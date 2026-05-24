c69b038fc21264358259092c3424fb6cc1b0a6c7
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:54 +0900

    add key transparency service methods

--- src/main/java/com/sovereigncomm/service/KeyTransparencyService.java
+++ src/main/java/com/sovereigncomm/service/KeyTransparencyService.java
@@ -3,3 +3,7 @@ package com.sovereigncomm.service;
 import java.util.Map;
 import java.util.UUID;
 
+public interface KeyTransparencyService {
+    void appendKeyEvent(UUID deviceId, String canonicalEntryJson, String entrySignatureBase64);
+    Map<String, Object> proof(UUID userId);
+}
