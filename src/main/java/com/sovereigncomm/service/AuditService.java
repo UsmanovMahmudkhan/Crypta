ab986f949ceab77143285d04639f0e45ea149553
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:10:37 +0000

    add AuditService lines 6-10

--- src/main/java/com/sovereigncomm/service/AuditService.java
+++ src/main/java/com/sovereigncomm/service/AuditService.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.AuditExportRequest;
 
 import java.util.Map;
+
+public interface AuditService {
+    void appendSecurityEvent(String eventType, Map<String, Object> metadata);
+    void exportAudit(AuditExportRequest request);
+}
