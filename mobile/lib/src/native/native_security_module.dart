d233b5b4731a0eaad13d3d8c90d36ba06d26794a
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:42:28 +0900

    add secure storage provider contract

--- mobile/lib/src/native/native_security_module.dart
+++ mobile/lib/src/native/native_security_module.dart
@@ -3,3 +3,8 @@ import 'dart:typed_data';
 abstract interface class SecureStorageProvider {
   Future<Uint8List> wrapKey({
     required String hardwareKeyAlias,
+    required Uint8List keyMaterial,
+  });
+
+  Future<Uint8List> unwrapKey({
+    required String hardwareKeyAlias,
