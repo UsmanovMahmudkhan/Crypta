ccd7c2d77a892d870ea7cd78e776caaa5fcb87d0
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:42:43 +0900

    add device controller imports

--- src/main/java/com/sovereigncomm/api/DeviceController.java
+++ src/main/java/com/sovereigncomm/api/DeviceController.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.api;
 import com.sovereigncomm.api.dto.CommonDtos.DeviceRegisterRequest;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.service.DeviceTrustService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
