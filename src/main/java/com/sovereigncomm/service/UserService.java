3e6e79157885d9c5fb9f33f6e03cdcf427c5cf22
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:44:30 +0900

    add user service methods

--- src/main/java/com/sovereigncomm/service/UserService.java
+++ src/main/java/com/sovereigncomm/service/UserService.java
@@ -3,3 +3,6 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.api.dto.CommonDtos.UserRegisterRequest;
 
+public interface UserService {
+    IdResponse registerUser(UserRegisterRequest request);
+}
