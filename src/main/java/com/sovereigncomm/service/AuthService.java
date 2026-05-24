c137cd711fa1f62488c80091683794c140c79df6
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:41 +0900

    add auth service methods

--- src/main/java/com/sovereigncomm/service/AuthService.java
+++ src/main/java/com/sovereigncomm/service/AuthService.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.WebAuthnFinishRequest;
 import com.sovereigncomm.api.dto.CommonDtos.WebAuthnStartResponse;
 
+import java.util.UUID;
+
+public interface AuthService {
+    WebAuthnStartResponse startWebAuthnRegistration(UUID userId);
+    void finishWebAuthnRegistration(WebAuthnFinishRequest request);
