a56dd56c3e9717daa8a48a45b776e7be9c54a93d
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:10:51 +0000

    add KeyService lines 5-9

--- src/main/java/com/sovereigncomm/service/KeyService.java
+++ src/main/java/com/sovereigncomm/service/KeyService.java
@@ -2,3 +2,8 @@ package com.sovereigncomm.service;
 
 import com.sovereigncomm.api.dto.CommonDtos.KeyBundleResponse;
 import com.sovereigncomm.api.dto.CommonDtos.PublicKeyUploadRequest;
+
+import java.util.UUID;
+
+public interface KeyService {
+    void uploadIdentityKey(PublicKeyUploadRequest request);
