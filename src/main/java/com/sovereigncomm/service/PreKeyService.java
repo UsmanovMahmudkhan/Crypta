30d075382244cb0fcc38b5268d1c5ddc66b90f1e
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:11:36 +0000

    add PreKeyService lines 6-9

--- src/main/java/com/sovereigncomm/service/PreKeyService.java
+++ src/main/java/com/sovereigncomm/service/PreKeyService.java
@@ -3,3 +3,7 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.PreKeyUploadRequest;
 
 public interface PreKeyService {
+    void uploadSignedPreKey(PreKeyUploadRequest request);
+    void uploadOneTimePreKey(PreKeyUploadRequest request);
+    void uploadPqPreKey(PreKeyUploadRequest request);
+}
