e61f68041b22df2f3e8e8aad72cb9be0e846ed0a
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:02 +0900

    add web authn controller imports

--- src/main/java/com/sovereigncomm/api/WebAuthnController.java
+++ src/main/java/com/sovereigncomm/api/WebAuthnController.java
@@ -3,3 +3,8 @@ package com.sovereigncomm.api;
 import com.sovereigncomm.api.dto.CommonDtos.WebAuthnFinishRequest;
 import com.sovereigncomm.api.dto.CommonDtos.WebAuthnStartResponse;
 import com.sovereigncomm.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
