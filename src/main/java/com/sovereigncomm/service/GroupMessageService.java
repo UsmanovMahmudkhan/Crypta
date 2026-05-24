cfa2c19a1caaef10a8524d0f0176959117eaee1b
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-25 00:28:42 +0900

    refactor GroupMessageService for improved concurrency

--- src/main/java/com/sovereigncomm/service/GroupMessageService.java
+++ src/main/java/com/sovereigncomm/service/GroupMessageService.java
@@ -1,5 +1,9 @@
 package com.sovereigncomm.service;
 
+import com.sovereigncomm.api.dto.CommonDtos.EncryptedMessageSubmitRequest;
+import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
+
 public interface GroupMessageService {
     void acceptCiphertextEnvelope(String encryptedGroupMessageEnvelope);
+    IdResponse submit(EncryptedMessageSubmitRequest request);
 }
