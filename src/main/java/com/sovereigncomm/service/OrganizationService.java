b46a6f71781b302bd979e13dbb184f43de28fc29
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:11:34 +0000

    add OrganizationService lines 6-8

--- src/main/java/com/sovereigncomm/service/OrganizationService.java
+++ src/main/java/com/sovereigncomm/service/OrganizationService.java
@@ -3,3 +3,6 @@ package com.sovereigncomm.service;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.api.dto.CommonDtos.OrganizationCreateRequest;
 
+public interface OrganizationService {
+    IdResponse createOrganization(OrganizationCreateRequest request);
+}
