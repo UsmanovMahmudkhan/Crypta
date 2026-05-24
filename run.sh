13947c1f373659c06c4cbb0cf5e34f77753338e7
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 20:40:46 +0900

    add run helper lines 6-10

--- run.sh
+++ run.sh
@@ -3,3 +3,8 @@
 # ANSI Color codes for beautiful console logging
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0;m' # No Color
+
+echo -e "${BLUE}======================================================${NC}"
