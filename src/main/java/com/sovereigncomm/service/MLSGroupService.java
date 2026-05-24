336c6ca29e2d2b77492bb1018c3994de6984f631
UsmanovMahmudkhan <UsmanovMahmudkhan@users.noreply.github.com>
2026-05-24 09:10:58 +0000

    add MLSGroupService lines 5-9

--- src/main/java/com/sovereigncomm/service/MLSGroupService.java
+++ src/main/java/com/sovereigncomm/service/MLSGroupService.java
@@ -2,3 +2,8 @@ package com.sovereigncomm.service;
 
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.api.dto.CommonDtos.MemberChangeRequest;
+import com.sovereigncomm.api.dto.CommonDtos.RoomCreateRequest;
+
+public interface MLSGroupService {
+    IdResponse createMissionRoom(RoomCreateRequest request);
+    void inviteMember(MemberChangeRequest request);
