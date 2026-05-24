f744300997b7f8cb84a3facb971e14819f84bfa0
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-25 00:14:13 +0900

    add endpoint to revoke device

--- src/main/java/com/sovereigncomm/api/DeviceController.java
+++ src/main/java/com/sovereigncomm/api/DeviceController.java
@@ -4,6 +4,8 @@ import com.sovereigncomm.api.dto.CommonDtos.DeviceRegisterRequest;
 import com.sovereigncomm.api.dto.CommonDtos.IdResponse;
 import com.sovereigncomm.service.DeviceTrustService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,10 @@ public class DeviceController {
     @PostMapping
     IdResponse register(@Valid @RequestBody DeviceRegisterRequest request) {
         return deviceTrustService.registerDevice(request);
+    }
+
+    @DeleteMapping("/{deviceId}")
+    void revoke(@PathVariable java.util.UUID deviceId) {
+        deviceTrustService.revokeDevice(deviceId, "api_request");
     }
 }
