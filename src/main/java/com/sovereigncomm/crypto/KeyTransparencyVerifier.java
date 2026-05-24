bbdb90e09c03decb816eb8a0532aaa335975d7cf
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-05-24 17:43:31 +0900

    define key transparency verifier contract

--- src/main/java/com/sovereigncomm/crypto/KeyTransparencyVerifier.java
+++ src/main/java/com/sovereigncomm/crypto/KeyTransparencyVerifier.java
@@ -2,3 +2,5 @@ package com.sovereigncomm.crypto;
 
 public interface KeyTransparencyVerifier {
     boolean verifyInclusionProof(byte[] canonicalEntry, byte[] inclusionProof, byte[] signedTreeHead);
+    boolean verifyConsistencyProof(byte[] oldSignedTreeHead, byte[] newSignedTreeHead, byte[] consistencyProof);
+}
